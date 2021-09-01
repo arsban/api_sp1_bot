3b974fa6dfee45a8b7457b1a4f7576a62c4e5abe
Георгий <georggeorguy@gmail.com>
2021-09-01 21:29:46 +0300

    исправления после ревью

--- homework.py
+++ homework.py
@@ -1,8 +1,8 @@
 import logging
 import os
 import time
+from json import JSONDecodeError
 from logging.handlers import RotatingFileHandler
-
 
 import requests
 import telegram
@@ -40,6 +40,8 @@ URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 SLEEP_TIME = (60 * 5)
 SLEEP_TIME_EXP = (5)
 
+LOG = "Log: "
+
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
@@ -54,10 +56,6 @@ def parse_homework_status(homework):
     }
     if homework_name is None:
         message: str = 'Работа не найдена.'
-        # Альтернативный путь для бесстрашных --
-        # поддержать Телеграм в своем логгере. © Алексей Пак
-        # не уверен, что это то что вы имели ввиду,
-        # но я всю ночь просидел и лучше не смог придумать. (дайте подсказку)
         logging.error(message)
         send_message(message)
     if homework_status in statuses:
@@ -73,23 +71,21 @@ def get_homeworks(current_timestamp):
     payload = {'from_date': current_timestamp}
     try:
         homework_statuses = requests.get(URL, headers=headers, params=payload)
+        return homework_statuses.json()
     except requests.RequestException as error:
         message: str = f'Ошибка {error}'
         logging.error(message, exc_info=True)
         send_message(message)
         return {}
-    except TypeError as error:
-        # тут тоже есть подозрение что ошибся,
-        # не смог найти сломанный json чтобы проверить, все гуглил
+    except JSONDecodeError as error:
         message: str = f'Невалидный формат {error}'
         logging.exception(message)
         send_message(message)
         return {}
-    return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(CHAT_ID, text=message)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
@@ -100,22 +96,22 @@ def main():
 
     while True:
         try:
-            try:
-                new_homework = get_homeworks(current_timestamp)
-                if new_homework.get('homeworks'):
-                    send_message(
-                        parse_homework_status(new_homework.get('homeworks')[0])
-                    )
-                current_timestamp = new_homework.get('current_date')
-                time.sleep(SLEEP_TIME)
-            except Exception as e:
-                message: str = f'Бот упал с ошибкой: {e}'
-                send_message(message)
-                logging.exception(message)
-                time.sleep(SLEEP_TIME_EXP)
-                continue
+            new_homework = get_homeworks(current_timestamp)
+            if new_homework.get('homeworks'):
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0])
+                )
+            current_timestamp = new_homework.get('current_date')
+            time.sleep(SLEEP_TIME)
+        except Exception as e:
+            message: str = f'Бот упал с ошибкой: {e}'
+            send_message(message)
+            logging.exception(message)
+            time.sleep(SLEEP_TIME_EXP)
+            continue
         except KeyboardInterrupt:
             break
+
     message: str = 'Бот отключён'
     send_message(message)
     logging.info(message)
